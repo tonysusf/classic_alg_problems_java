3ff3185877a04b3fb38619be8cf6f662cbadfbed
Tony Su <tonysuglobal@gmail.com>
2026-09-05 12:48:50 -0700

    two sum

---
+++ two_sum/TwoSum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> lookup = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (lookup.containsKey(target - nums[i])) {
+                return new int[] {lookup.get(target - nums[i]), i};
+            }
+            lookup.put(nums[i], i);
+        }
+
+        return new int[0];
+    }
+}
+
+
