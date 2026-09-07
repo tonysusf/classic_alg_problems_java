15e0fd425dbc86f3254789ee49c34e518ad40db6
Tony Su <tonysuglobal@gmail.com>
2026-09-06 18:51:12 -0700

    Java basics

---
+++ java_basics/JavaBasics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/*
+* javac JavaBasics.java; java JavaBasics
+*/
+
+class JavaBasics {
+
+    public static void main(String[] args) {
+        // Array
+        int[] arr = {1, 2, 8};
+        System.out.println("arr is " + Arrays.toString(arr));
+    }
+}
+
+
--- two_sum/TwoSum.java
+++ two_sum/TwoSum.java
@@ -1,8 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        System.out.println("Input is: " + Arrays.toString(nums) + ", target is: " + target);
         Map<Integer, Integer> lookup = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -13,6 +13,13 @@ class TwoSum {
         }
 
         return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 8, 12, 4};
+        TwoSum s = new TwoSum();
+        int[] result = s.twoSum(nums, 12);
+        System.out.println("result is " + Arrays.toString(result));
     }
 }
 
